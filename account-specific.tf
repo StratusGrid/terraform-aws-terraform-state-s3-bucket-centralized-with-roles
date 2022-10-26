01c4fe247c3a5f9715025c719ec90dde28ec2956
DJKaswa <dieudonne.kaswa@stratusgrid.com>
2022-10-26 14:22:07 -0500

    Corrects variable interpolation.

--- account-specific.tf
+++ account-specific.tf
@@ -12,7 +12,7 @@ resource "aws_kms_key" "specific_remote_state_backend" {
 resource "aws_kms_alias" "specific_state_backend" {
   count         = length(var.account_arns)
   name          = "alias/${var.name_prefix}-remote-state-backend-${replace(var.account_arns[count.index], local.account_from_arn, "$5")}${var.name_suffix}"
-  target_key_id = aws_kms_key.specific_remote_state_backend.*.key_id[count.index]
+  target_key_id = aws_kms_key.specific_remote_state_backend[*].key_id[count.index]
 }
 
 data "aws_iam_policy_document" "account_specific_policy" {
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "account_specific_policy" {
       "kms:ReEncrypt*"
     ]
     resources = [
-      aws_kms_key.specific_remote_state_backend.*.arn[count.index]
+      aws_kms_key.specific_remote_state_backend[*].arn[count.index]
     ]
     sid = "AllowUseOfRemoteStateBackendKMSKey"
   }
@@ -76,7 +76,7 @@ resource "aws_iam_policy" "account_state_policy" {
   count       = length(var.account_arns)
   name        = "${replace(var.account_arns[count.index], local.account_from_arn, "$5")}-terraform-state-assumed-policy"
   description = "Policy given upon role assumption of ${replace(var.account_arns[count.index], local.account_from_arn, "$5")}-terraform-state role"
-  policy      = data.aws_iam_policy_document.account_specific_policy.*.json[count.index]
+  policy      = data.aws_iam_policy_document.account_specific_policy[*].json[count.index]
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -98,11 +98,11 @@ data "aws_iam_policy_document" "assume_role_policy" {
 resource "aws_iam_role" "account_state_role" {
   count              = length(var.account_arns)
   name               = "${replace(var.account_arns[count.index], local.account_from_arn, "$5")}-terraform-state"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.*.json[count.index]
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy[*].json[count.index]
 }
 
 resource "aws_iam_role_policy_attachment" "account_state_role" {
   count      = length(var.account_arns)
-  role       = aws_iam_role.account_state_role.*.name[count.index]
-  policy_arn = aws_iam_policy.account_state_policy.*.arn[count.index]
+  role       = aws_iam_role.account_state_role[*].name[count.index]
+  policy_arn = aws_iam_policy.account_state_policy[*].arn[count.index]
 }
